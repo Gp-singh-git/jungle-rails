35a8d2d8dd092c3a16e9b56553e878f1ccb64b6d
gurpreet singh <gurpreet212004@gmail.com>
2021-09-16 09:58:16 +0000

    add http basic auth to admin

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
-
+# Add Env variables with authentication found in .env file
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
+#end
   def index
     @products = Product.order(id: :desc).all
   end
