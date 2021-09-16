0dc6b78904e546abed7a64bef4c35262db93e006
gurpreet singh <gurpreet212004@gmail.com>
2021-09-16 14:15:25 +0000

    added to dashboard product and category counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,28 @@
 class Admin::DashboardController < ApplicationController
+  # Display a count of how many products are in the database
+  # Display a count of how many categories are in the database
+  # def show
+  #   @product = Product.count
+  #   @category = Category.count
+  # end
 
   def show
+    @products = Product.count
+    @categories = Category.count
   end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :description,
+      :category_id,
+      :quantity,
+      :image,
+      :price
+    )
+  end
+
+
 end
